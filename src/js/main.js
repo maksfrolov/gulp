53b52b4c0d6b66aaed7149397a6d05b854e0b306
maksfrolov <be.your.side@yandex.ru>
2019-07-23 20:12:58 +0400

    delay

--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,17 @@
-var q = 'Hello world';
+var q = 'one';
+var w;
+var e = 0;
+var r = true;
+var t = prompt('деление на 2', 0);
+var y = t/2;
 console.log(q);
+console.log(typeof q);
+console.log(w);
+console.log(typeof w);
+console.log(e);
+console.log(typeof e);
+alert(t);
+console.log(typeof r);
+alert(y);
+console.log(typeof y);
+
