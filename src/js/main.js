a5141ca9e0f967677c4912dcd566236a51e55f3d
maksfrolov <be.your.side@yandex.ru>
2019-07-24 15:07:06 +0400

    lals

--- src/js/main.js
+++ src/js/main.js
@@ -1,17 +1,18 @@
-var q = 'one';
-var w;
-var e = 0;
-var r = true;
-var t = prompt('деление на 2', 0);
-var y = t/2;
+var q = 'Hello';
+var w = 123;
+var e = false;
+var r = prompt('умножение на 2', 2);
+var t = r*r;
+var u;
 console.log(q);
 console.log(typeof q);
 console.log(w);
 console.log(typeof w);
 console.log(e);
 console.log(typeof e);
-alert(t);
+console.log(typeof u);
+alert(r);
 console.log(typeof r);
-alert(y);
+alert(t);
 console.log(typeof y);
 
